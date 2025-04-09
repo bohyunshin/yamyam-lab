7734a6de99a4f0e015fa6b95b5c3fa48e72bc8ca
ds wook <46340424+ds-wook@users.noreply.github.com>
2025-04-09 20:07:32 +0900

    fix/add try except code (#135)

--- src/train_ranker.py
+++ src/train_ranker.py
@@ -26,7 +26,7 @@ def main(cfg: DictConfig):
         test=cfg.data.test,
     )
     data = data_loader.prepare_train_val_dataset(
-        is_rank=True, is_candidate_dataset=True
+        is_rank=True, is_candidate_dataset=cfg.data.is_candidate_dataset
     )
     # mapping reverse
     X_train, y_train, X_test, y_test = (
@@ -36,96 +36,109 @@ def main(cfg: DictConfig):
         data["y_val"],
     )
 
-    candidates = data["candidates"]
-    reviewer_mapping = {v: k for k, v in data["user_mapping"].items()}
-    candidate_mapping = {v: k for k, v in data["candidate_user_mapping"].items()}
-
-    X_test["reviewer_id"] = X_test["reviewer_id"].map(reviewer_mapping)
-    candidates["reviewer_id"] = candidates["reviewer_id"].map(candidate_mapping)
-
-    # train model
+    # build Pmodel
     trainer = build_model(cfg)
 
-    # train model
-    trainer.fit(X_train, y_train, X_test, y_test)
+    try:
+        candidates = data["candidates"]
+        reviewer_mapping = {v: k for k, v in data["user_mapping"].items()}
+        diner_mapping = {v: k for k, v in data["diner_mapping"].items()}
+        candidate_mapping = {v: k for k, v in data["candidate_user_mapping"].items()}
+        candidate_diner_mapping = {
+            v: k for k, v in data["candidate_diner_mapping"].items()
+        }
+
+        X_test["reviewer_id"] = X_test["reviewer_id"].map(reviewer_mapping)
+        X_test["diner_idx"] = X_test["diner_idx"].map(diner_mapping)
+        candidates["reviewer_id"] = candidates["reviewer_id"].map(candidate_mapping)
+        candidates["diner_idx"] = candidates["diner_idx"].map(candidate_diner_mapping)
+
+        # train model
+        trainer.fit(X_train, y_train, X_test, y_test)
+
+        # candidate predictions
+        batch_size = 100000
+        num_batches = (len(candidates) + batch_size - 1) // batch_size
+        predictions = np.zeros(len(candidates))
+
+        for i in tqdm(range(num_batches)):
+            start_idx = i * batch_size
+            end_idx = min((i + 1) * batch_size, len(candidates))
+            batch = candidates[cfg.data.features].iloc[start_idx:end_idx]
+            predictions[start_idx:end_idx] = trainer.predict(batch)
+
+        # Group predictions by user
+        candidates["pred_score"] = predictions
+        candidates = candidates.sort_values(
+            by=["reviewer_id", "pred_score"], ascending=False
+        )
+        user_predictions = candidates.groupby("reviewer_id")["diner_idx"].apply(
+            np.array
+        )
+
+        # Calculate metrics
+        metric_at_K = {K: {"map": 0, "ndcg": 0, "count": 0} for K in [3, 7, 10, 20]}
+
+        # Get already liked items from test data
+        test_liked_items = X_test.groupby("reviewer_id")["diner_idx"].apply(np.array)
+
+        # Get already liked items from training data
+        train_liked_items = X_train.groupby("reviewer_id")["diner_idx"].apply(np.array)
+
+        # Calculate metrics for each user
+        for user in tqdm(user_predictions.index):
+            if user not in test_liked_items:
+                continue
+
+            liked_items = test_liked_items[user]
+            pred_items = user_predictions[user]
+
+            # Filter out already liked items from training data
+            if user in train_liked_items:
+                already_liked = train_liked_items[user]
+                # Create a mask for items that were not liked in training
+                mask = ~np.isin(pred_items, already_liked)
+                pred_items = pred_items[mask]
+
+            # Skip if no predictions left after filtering
+            if len(pred_items) == 0:
+                continue
+
+            for K in metric_at_K.keys():
+                # if len(liked_items) < K:
+                #     continue
+
+                metric = ranking_metrics_at_k(
+                    liked_items=np.array(liked_items), reco_items=pred_items[:K]
+                )
+
+                metric_at_K[K]["map"] += metric["ap"]
+                metric_at_K[K]["ndcg"] += metric["ndcg"]
+                metric_at_K[K]["count"] += 1
+
+        # Average metrics
+        table = PrettyTable()
+        table.field_names = ["K", "MAP", "NDCG"]
+
+        for K in metric_at_K:
+            metric_at_K[K]["map"] = safe_divide(
+                numerator=metric_at_K[K]["map"], denominator=metric_at_K[K]["count"]
+            )
+            metric_at_K[K]["ndcg"] = safe_divide(
+                numerator=metric_at_K[K]["ndcg"], denominator=metric_at_K[K]["count"]
+            )
+            table.add_row(
+                [K, f"{metric_at_K[K]['map']:.8f}", f"{metric_at_K[K]['ndcg']:.8f}"]
+            )
+
+        logging.info(f"\nEvaluation Results\n{table}")
+
+    except Exception:
+        logging.info("No candidates data found just training model")
+        trainer.fit(X_train, y_train, X_test, y_test)
 
     # save model
     trainer.save_model()
-
-    # candidate predictions
-    batch_size = 100000
-    num_batches = (len(candidates) + batch_size - 1) // batch_size
-    predictions = np.zeros(len(candidates))
-
-    for i in tqdm(range(num_batches)):
-        start_idx = i * batch_size
-        end_idx = min((i + 1) * batch_size, len(candidates))
-        batch = candidates[cfg.data.features].iloc[start_idx:end_idx]
-        predictions[start_idx:end_idx] = trainer.predict(batch)
-
-    # Group predictions by user
-    candidates["pred_score"] = predictions
-    candidates = candidates.sort_values(
-        by=["reviewer_id", "pred_score"], ascending=False
-    )
-    user_predictions = candidates.groupby("reviewer_id")["diner_idx"].apply(np.array)
-
-    # Calculate metrics
-    metric_at_K = {K: {"map": 0, "ndcg": 0, "count": 0} for K in [3, 7, 10, 20]}
-
-    # Get already liked items from test data
-    test_liked_items = X_test.groupby("reviewer_id")["diner_idx"].apply(np.array)
-
-    # Get already liked items from training data
-    train_liked_items = X_train.groupby("reviewer_id")["diner_idx"].apply(np.array)
-
-    # Calculate metrics for each user
-    for user in tqdm(user_predictions.index):
-        if user not in test_liked_items:
-            continue
-
-        liked_items = test_liked_items[user]
-        pred_items = user_predictions[user]
-
-        # Filter out already liked items from training data
-        if user in train_liked_items:
-            already_liked = train_liked_items[user]
-            # Create a mask for items that were not liked in training
-            mask = ~np.isin(pred_items, already_liked)
-            pred_items = pred_items[mask]
-
-        # Skip if no predictions left after filtering
-        if len(pred_items) == 0:
-            continue
-
-        for K in metric_at_K.keys():
-            if len(liked_items) < K:
-                continue
-
-            metric = ranking_metrics_at_k(
-                liked_items=np.array(liked_items), reco_items=pred_items[:K]
-            )
-
-            metric_at_K[K]["map"] += metric["ap"]
-            metric_at_K[K]["ndcg"] += metric["ndcg"]
-            metric_at_K[K]["count"] += 1
-
-    # Average metrics
-    table = PrettyTable()
-    table.field_names = ["K", "MAP", "NDCG"]
-
-    for K in metric_at_K:
-        metric_at_K[K]["map"] = safe_divide(
-            numerator=metric_at_K[K]["map"], denominator=metric_at_K[K]["count"]
-        )
-        metric_at_K[K]["ndcg"] = safe_divide(
-            numerator=metric_at_K[K]["ndcg"], denominator=metric_at_K[K]["count"]
-        )
-        table.add_row(
-            [K, f"{metric_at_K[K]['map']:.8f}", f"{metric_at_K[K]['ndcg']:.8f}"]
-        )
-
-    logging.info(f"\nEvaluation Results\n{table}")
 
     # plot feature importance
     trainer.plot_feature_importance()
